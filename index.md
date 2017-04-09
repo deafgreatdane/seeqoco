b26c9098831d939c702bf8759f7706647c0157e9
Ben Johnson <ben.johnson@seeq.com>
2017-04-09 10:34:58 -0500

    with docs

--- index.md
+++ index.md
@@ -1,1 +1,38 @@
-Hello.
+# Seeqoco 
+This site contains a userscript for Tamper Monkey that modifies Sococo with some fun additions.
+
+Pull requests for new configs and additional features are welcome.
+## One time setup
+You probably want a site-specific browser, see [epichrome](https://github.com/dmarmor/epichrome). This doesn't work with the Sococo native app.
+
+Install Tampermonkey plugin
+
+Import the user script with the following url:
+
+https://deafgreatdane.github.io/seeqoco/seeqoco.user.js
+
+## Customizations
+Each person is configured in the `officeConfiguration` object. Required elements:
+
+* email - This isn't used for anything yet, so we can assign fake emails to non-offices
+* room - This is the ID of your sococo office. Get this from the inspector
+
+# Features
+## Office Decorations
+This config goes in the `officeDecorations` object. It is an array of objects with the following attributes:
+
+* type - one of "carpet" "furniture"
+* css - The css that describes the object. You probably want at least the following:
+ * background-image
+ * width, height
+ * top, left
+
+## Pets
+Of course you can bring your pets to work! But pets have a tendency to wander, so some locations might need to be off limits.
+ 
+Adding the `animals` option to your office indicates you're a pet-friendly  location.
+
+A suboption is the `pets` array. Each pet is configured with:
+
+* url - the full url to an image. Pets are automatically constrained to 25x25
+* name - Maybe you you don't know everyone's animal, so this enables you to hover over the image to learn their name. 
--- seeqoco.user.js
+++ seeqoco.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Seeqoco
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.5
 // @description  Add some fun to the virtual office space
 // @author       ben.johnson@seeq.com
 // @match        https://app.sococo.com/*
@@ -19,21 +19,23 @@ var officeSettings = [
           "css": {"background-image":"url(https://deafgreatdane.github.io/seeqoco/images/couch.png)",
                   "background-size":"50px",
                   "top": 2,"width": "50px",
-                  "height":"80px","left":"8px"}},    
+                  "height":"80px","left":"8px"}},
          { "type": "furniture",
-          "css": {"background-image":"url(https://deafgreatdane.github.io/seeqoco/images/greatdane.png)",
-                  "background-size":"30px",
-                  "top": 32,"width": "50px",
-                  "height":"30px","left":"20px"}},
-          { "type": "furniture",
           "css": {"background-image":"url(https://deafgreatdane.github.io/seeqoco/images/deer.png)",
                   "background-size":"35px",
                   "top": 22,"width": "50px",
                   "height":"80px","left":"120px"}}
-     ]
+     ],
+     animals: {
+         pets: [ { "name":"costner",
+                  "url":"https://deafgreatdane.github.io/seeqoco/images/greatdane.png"
+                 }
+               ]
+     }
     },
     { "email":"dakota.kanner@seeq.com" ,
      "room": "room-2",
+     animals: { },
      "officeDecorations": [
          { "type":"carpet" ,
           "westWall":true,
@@ -43,6 +45,7 @@ var officeSettings = [
     },
     { "email":"vacant corner" ,
      "room": "room-38",
+     animals: {},
      "officeDecorations": [
          { "type":"carpet" ,
           "westWall":true,
@@ -50,9 +53,106 @@ var officeSettings = [
           "css" : { "background-color":"green"}
          }
      ]
+    },
+    {email: "lobby",
+     "name":"Lobby",
+     "room":"room-1",
+     animals:{foo:1}},
+    {email:'kitchen',
+     name:"Kitchen",
+     room:"room-7",
+     animals:{foo:1}
     }
 ];
 
+function SeeqocoAnimals() {
+    this.animalHangouts = [];
+    // the max number of seconds a pet will hangout in one room.
+    // they'll always visit at least 1/2 this time
+    this.hangoutDuration = 10;
+}
+SeeqocoAnimals.prototype.deferredMove = function(petEl, pet) {
+    if ( pet.wanderlust == 0 ) {
+        return;
+    }
+    var self = this;
+    var retval = function() {
+        var roomNumber = Math.floor(Math.random()* seeqoco.animals.animalHangouts.length);
+        seeqoco.animals.animalHangouts[roomNumber].prepend(petEl);
+        self.deferredMove(petEl,pet);
+    };
+    
+    var stayStill = (1 + Math.random())/2 * this.hangoutDuration * 1000;
+    window.setTimeout(retval,stayStill);
+};
+SeeqocoAnimals.prototype.setupRoom = function(office,beforeElement) {
+    if ( ! office.hasOwnProperty('animals')) {
+        return;
+    }
+    var el = $("<div></div>");
+    el.css({'position':'absolute','width':60,'height':25, top: 45});
+    this.animalHangouts[this.animalHangouts.length] = el;
+
+    if ( office.animals.hasOwnProperty('pets')) {
+        _.forEach(office.animals.pets, function(pet) {
+            if ( ! pet.hasOwnProperty('wanderlust')) {
+                pet.wanderlust = 2;
+            }
+            var petEl = $("<img>");
+            this.deferredMove(petEl,pet);
+            petEl.prop('src',pet.url);
+            petEl.prop('title',pet.name);
+            petEl.prop('width','25');
+            petEl.prop('height','25');
+            el.prepend(petEl);
+        },this);
+    }
+    beforeElement.before(el);
+};
+
+function Seeqoco() {
+    // the list of rooms that allow animals
+    this.animals = new SeeqocoAnimals();
+
+    this.newOffice = function(office) {
+        console.log("sqc: S " +office.email);
+        if ( office.hasOwnProperty('room') ) {
+            var room = $("DIV[data-room-id='" + office.room + "']");
+            var firstChild = room.find(":first-child");
+            if ( office.hasOwnProperty('officeDecorations') ) {
+                _.forEach(office.officeDecorations, function(deco) {
+                    var el = $("<div></div>");
+                    el.css('position','absolute');
+                    el.width(20);
+                    el.height(20);
+                    if ( roomDecorations.hasOwnProperty(deco.type)) {
+                        roomDecorations[deco.type](room, office, el, deco);
+                    }
+                    if( deco.hasOwnProperty('css')) {
+                        el.css(deco.css);
+                    }
+                    firstChild.before(el);
+                });
+            }
+            this.animals.setupRoom(office,firstChild);
+        }
+    };
+
+    this.initDone = false;
+
+    this.primarySetup = function() {
+        if ( this.initDone) {
+            return;
+        } 
+        this.initDone = true;
+        _.forEach(officeSettings, this.newOffice, this);
+        console.log("sqc: done");
+    };
+
+    return this;
+};
+
+var seeqoco = new Seeqoco();
 // /////////////// helper methods for configuration
 var roomDecorations = {
     carpet: function(roomEl, roomConfig, el, options) {
@@ -69,6 +169,7 @@ var roomDecorations = {
         }
         el.width(width);
         el.height(height);
+        el.css('z-index',0);
         el.css('left',left);
         el.css('top',top);
     },
@@ -81,46 +182,17 @@ var roomDecorations = {
 };
 
 // ///////////////////////////////////////  setup
-$(function() {
-    window.setTimeout(waitToLoad,3000);
-});
-
-function waitToLoad() {
-    console.log("sqc: checking load");
-    var room = $("DIV[data-room-id='room-1']");
-    if ( room.height() <= 0 ) {
-        window.setTimeout(waitToLoad,1000);
-        return;
-    }
-    tweakThings();
-}
-function tweakThings() {
-    _.forEach(officeSettings, function(office) {
-        console.log("sqc: " +office.email);
-        if ( office.hasOwnProperty('room') ) {
-            if ( office.hasOwnProperty('officeDecorations') ) {
-                _.forEach(office.officeDecorations.reverse(), function(deco) {
-                    var room = $("DIV[data-room-id='" + office.room + "']");
-                    var el = $("<div></div>");
-                    el.css('position','absolute');
-                    el.width(20);
-                    el.height(20);
-                    if ( roomDecorations.hasOwnProperty(deco.type)) {
-                        roomDecorations[deco.type](room, office, el, deco);
-                    }
-                    if( deco.hasOwnProperty('css')) {
-                        el.css(deco.css);
-                    }
-                    room.prepend(el);
-                });
-            }
-            if ( office.hasOwnProperty('animals')) {
-                    var room = $("DIV[data-room-id='" + office.room + "']");
-                    var el = $("<div></div>");
-                    el.css('position','absolute');
-                
-            }
+(function() {
+    function waitToLoad() {
+        console.log("sqc: checking load");
+        var room = $("DIV[data-room-id='room-1']");
+        if ( room.height() <= 0 ) {
+            window.setTimeout(waitToLoad,1000);
+            return;
         }
+        seeqoco.primarySetup();
+    }
+    $(function() {
+        window.setTimeout(waitToLoad,3000);
     });
-    console.log("sqc: done");
-}
+})();
